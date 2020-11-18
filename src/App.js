fe17579eb38e8467d5a09ac89b7e6a5825fe7a17
rick237518 <67753269+rick237518@users.noreply.github.com>
2020-11-18 18:09:05 +0100

    deploy my first app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import profileImage from './img/b.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My name is ricardo</h1>
+      <h2>thanks</h2>
+      <img src={profileImage} alt=""/>
+      
+    </div>
+  );
+}
+
+export default App;
